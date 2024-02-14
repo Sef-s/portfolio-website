6e6394aa31821048167ecac5a21f8d033cb79b95
Yusef Tohamy <yusef.t11@gmail.com>
2024-02-14 15:25:51 -0500

    Updated Portfolio

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,30 +1,40 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
 const [letterClass, setLetterClass] = useState('text-animate')
 const nameArray = ['y', 'u', 's', 'e', 'f']
-const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r', '.']
+const jobArray = ['a', ' ', 's', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r', '.']
+
+useEffect(() => {
+    const timer = setTimeout(() => {
+        setLetterClass('text-animate-hover');
+    }, 4000); // Adjust the timeout duration as needed
+
+    return () => clearTimeout(timer);
+}, []);
+
 
     return (
         <div className="container home-page"> 
             <div className="text-zone">
                 <h1>
                 <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _13`}>,</span>
                 <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14 text-animate-m`}>'m</span>
-                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                <span className={`${letterClass} _14`}>I</span>
+                <span className={`${letterClass} _15`}>'</span>
+                <span className={`${letterClass} _16 text-animate-m`}>m</span>
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={17} />
                 <br />
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20} />
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={18} />
                 </h1>
                 <h2>Backend Developer / Python Engineer / Entrepreneur</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-
         </div>
     );
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,23 +7,11 @@ const Layout = () => {
     <div className="App">
       <Sidebar />
       <div className="page">
-        <span className="tags top-tags fade-in">
-          <strong>&lt;body&gt;</strong> {/* Making <body> tag bold and fade in */}
-        </span>
-
         <Outlet />
-        <span className="tags bottom-tags fade-in">
-          <strong>&lt;/body&gt;</strong> {/* Making </body> tag bold and fade in */}
           <br />
-          <span className="bottom-tag-html fade-in">
-            <strong>&lt;/html&gt;</strong> {/* Making </html> tag bold and fade in */}
-          </span>
-        </span>
       </div>
     </div>
   )
 }
-
-
 
 export default Layout
