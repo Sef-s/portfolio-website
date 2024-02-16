0437cb0ab770cfebce6c5fb565f1a3d1d0dfb1cf
Yusef Tohamy <yusef.t11@gmail.com>
2024-02-15 21:10:22 -0500

    Refactor contact and resume links in Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -44,8 +44,13 @@ useEffect(() => {
                 <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={18} />
                 </h1>
                 <h2>Backend Developer / Python Engineer / Entrepreneur</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-                <a href={Resume} className='flat-button2' download="YusefTohamy-Resume.pdf">RESUME</a>
+                <Link to="/contact" className="contact-link" role="button">
+                    <span class="text">CONTACT ME</span>
+                </Link>
+                <a href={Resume} className="resume-link" download="YusefTohamy-Resume.pdf" role="button">
+                    <span class="text">RESUME</span>
+                </a>
+
             </div>
             <div class="card">
                 <div class="card__content">
