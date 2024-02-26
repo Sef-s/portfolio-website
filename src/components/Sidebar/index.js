fd98d1a78d955f2201456e133c4d59e38dd9226d
Yusef Tohamy <yusef.t11@gmail.com>
2024-02-26 11:23:35 -0500

    Update color codes in SCSS files

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,37 +13,37 @@ const Sidebar = () => (
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-                    <FontAwesomeIcon icon={faHome} color="white" />
+                    <FontAwesomeIcon icon={faHome} color="#FFFFF0" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills">
-                    <FontAwesomeIcon icon={faGear} color="white" />
+                    <FontAwesomeIcon icon={faGear} color="#FFFFF0" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                    <FontAwesomeIcon icon={faEnvelope} color="white" />
+                    <FontAwesomeIcon icon={faEnvelope} color="#FFFFF0" />
             </NavLink>  
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                    <FontAwesomeIcon icon={faUser} color="white" />
+                    <FontAwesomeIcon icon={faUser} color="#FFFFF0" />
             </NavLink> 
         </nav>
         <ul>
             <li>
                 <a target="_blank" rel='noreferer' href='https://www.linkedin.com/in/yuseftohamy/'>
-                    <FontAwesomeIcon icon={faLinkedin} color="white" />
+                    <FontAwesomeIcon icon={faLinkedin} color="#FFFFF0" />
                 </a>
             </li>
             <li>
                 <a target="_blank" rel='noreferer' href='https://github.com/Sef-s'>
-                    <FontAwesomeIcon icon={faGithub} color="white" />
+                    <FontAwesomeIcon icon={faGithub} color="#FFFFF0" />
                 </a>
             </li>
             <li>
                 <a target="_blank" rel='noreferer' href='https://www.instagram.com/yusef.t/'>
-                    <FontAwesomeIcon icon={faInstagram} color="white" />
+                    <FontAwesomeIcon icon={faInstagram} color="#FFFFF0" />
                 </a>
             </li>
             <li>
                 <a target="_blank" rel='noreferer' href='https://open.spotify.com/user/0hzbdvn9dm263s5fdfbg3h5be'>
-                    <FontAwesomeIcon icon={faSpotify} color="white" />
+                    <FontAwesomeIcon icon={faSpotify} color="#FFFFF0" />
                 </a>
             </li>
         </ul>
